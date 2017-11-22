40fda810ab7af46fa710b4e189e1efef84f4ee14
johanblumenberg <johan.blumenberg@gmail.com>
2017-11-22 13:57:10 +0100

    Adding support for css sourcemaps

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var gutil = require('gulp-util');
 var through = require('through2');
 var _ = require('lodash');
 var rework = require('rework');
+var applySourceMap = require('vinyl-sourcemaps-apply');
+
 var lastIsObject = _.flowRight(_.isPlainObject, _.last);
 
 module.exports = function () {
@@ -24,7 +26,26 @@ module.exports = function () {
 		try {
 			var ret = rework(file.contents.toString(), {source: file.path});
 			plugins.forEach(ret.use.bind(ret));
-			file.contents = new Buffer(ret.toString(options));
+
+			if (file.sourceMap) {
+				options.sourcemap = true;
+				options.inputSourcemap = false;
+				options.sourcemapAsObject = true;
+			}
+			var result = ret.toString(options);
+			if (file.sourceMap) {
+				file.contents = new Buffer(result.code);
+
+				result.map.file = file.relative;
+				result.map.sources = result.map.sources.map(function (src) {
+					return file.relative;
+				});
+
+				applySourceMap(file, result.map);
+			} else {
+				file.contents = new Buffer(result);				
+			}
+
 			cb(null, file);
 		} catch (err) {
 			cb(new gutil.PluginError('gulp-rework', err, {fileName: err.filename || file.path}));
